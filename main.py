245b209d01d851c1cc44834ac0be86b2dd4b7a50
eclipse4573 <sehgal.tanishka3@gmail.com>
2025-03-15 22:15:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ Give me a tweet on {topic} in {language}.
 Please follow the below instructions:
 1. Do not translate to English if the given language is not English.
 2. The maximum word limit is 15 words.
-3. If {lang} is not a real language, default to English and ignore {lang}.
+3. If {language} is empty or not a real language, default to English and ignore {language}.
+4. If {topic} is empty or does not make sense, then respond with "Please enter a topic for your tweet!".
 """
-tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['topic', 'language', 'lang'])
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['topic', 'language'])
 
 topic = st.text_input("Topic: ")
 number = st.number_input("Number of Tweets: ", value = 1, step = 1, max_value = 10, min_value = 1)
@@ -24,6 +25,5 @@ if st.button("Generate"):
     tweet_chain = tweet_prompt | model
     for i in range(number):
         response = tweet_chain.invoke({"topic": topic,
-                                       "language": language,
-                                       "lang": language})
+                                       "language": language,})
         st.code(response.content)
