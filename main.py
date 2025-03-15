9447dec60d99a12fbc70b22c46dd35f246eac0f1
eclipse4573 <sehgal.tanishka3@gmail.com>
2025-03-15 22:23:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 model = Google(model = "gemini-1.5-flash-latest")
 
 tweet_template = """
-Give me a tweet on {topic} in {language}.
+Give me {number} tweets on {topic} in {language}.
 Please follow the below instructions:
 1. Do not translate to English if the given language is not English.
 2. The maximum word limit is 15 words.
 3. If {language} is empty or not a real language, default to English and ignore {language}.
 4. If {topic} is empty or does not make sense, then respond with "Please enter a topic for your tweet!".
 5. Be creative with every tweet!
-6. Do not repeat tweets.
 """
-tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['topic', 'language'])
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic', 'language'])
 
 with st.form(key = 'tweets'):
     topic = st.text_input("Topic: ")
@@ -28,7 +27,7 @@ with st.form(key = 'tweets'):
 
 if submit:
     tweet_chain = tweet_prompt | model
-    for i in range(number):
-        response = tweet_chain.invoke({"topic": topic,
-                                       "language": language,})
-        st.code(response.content, language = None)
+    response = tweet_chain.invoke({"number": number,
+                                   "topic": topic,
+                                   "language": language,})
+    st.code(response.content, language = None)
