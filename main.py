5feb310a340d67465ef1b7b7959d84508fd704a3
eclipse4573 <sehgal.tanishka3@gmail.com>
2025-03-15 22:18:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ st.title('Tweet Generator')
 
 os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 model = Google(model = "gemini-1.5-flash-latest")
+
 tweet_template = """
 Give me a tweet on {topic} in {language}.
 Please follow the below instructions:
@@ -17,11 +18,12 @@ Please follow the below instructions:
 """
 tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['topic', 'language'])
 
-topic = st.text_input("Topic: ")
-number = st.number_input("Number of Tweets: ", value = 1, step = 1, max_value = 10, min_value = 1)
-language = st.text_input("Language: ")
+with st.form(key = 'tweets'):
+    topic = st.text_input("Topic: ")
+    number = st.number_input("Number of Tweets: ", value = 1, step = 1, max_value = 10, min_value = 1)
+    language = st.text_input("Language: ")
 
-if st.button("Generate"):
+if st.form_submit_button("Generate"):
     tweet_chain = tweet_prompt | model
     for i in range(number):
         response = tweet_chain.invoke({"topic": topic,
